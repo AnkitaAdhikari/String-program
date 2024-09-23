00e86606105170bdd546fde3719153e54e3b797b
Ankita <Ankita@192.168.1.7>
2024-09-23 20:01:27 +0530

    Insert all files

---
+++ First_String_program/src/concat_string_program/concat_string_program.java
@@ -0,0 +1,9 @@
+package concat_string_program;
+
+public class concat_string_program {
+	public static void main(String[]args) {
+		String a="Ankita";
+		String b="Adhikari";
+		System.out.println(a+" "+b);
+	}
+}
